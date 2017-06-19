9f3234704c37155ea1d27b71b769fde63cbe6bd1
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-06-19 14:59:02 -0400

    Create Pascal's Triangle.java

---
+++ Algorithm/Pascal's Triangle.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        if(numRows <= 0){
+            return res;
+        }
+        
+        List<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        res.add(new ArrayList<>(temp));
+        
+        for(int i = 2; i <= numRows; i++){
+            List<Integer> r = new ArrayList<Integer>();    
+
+            r.add(1);
+            for(int j = 0; j < temp.size() - 1; j++){
+                r.add(temp.get(j) + temp.get(j + 1));
+            }
+            
+            r.add(1);
+            temp = r;
+            res.add(new ArrayList<>(temp));
+        }
+        
+        return res;
+        
+    }
+}
+
+public class Solution {
+
+    public List<Integer> getRow(int rowIndex) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        if (rowIndex < 0)
+            return result;
+
+        result.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = result.size() - 2; j >= 0; j--) {
+                result.set(j + 1, result.get(j) + result.get(j + 1));
+            }
+            result.add(1);
+        }
+        return result;
+    }
