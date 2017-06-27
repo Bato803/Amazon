64b0012c72b4d6ff75222218697ba40aa32ec35a
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-06-27 12:04:52 -0400

    Create Video.java

---
+++ Algorithm/Video.java
@@ -0,0 +1,119 @@
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+
+        User user = new User();
+
+
+        Inventory inventory = Inventory.getInstance();
+
+        Video tv = new TV();
+
+        inventory.map.put(1L, tv);
+
+        Video video = inventory.map.get(1L);
+
+        user.playVideo(video);
+
+        user.stopPlay(video);
+
+
+    }
+}
+
+
+class TV extends Video {
+
+
+    private double time;
+
+    @Override
+    void play(double offset) {
+        System.out.println("TV");
+    }
+
+    @Override
+    void stop() {
+        System.out.println("STOP");
+    }
+}
+
+abstract class Video {
+
+    long id;
+
+    String url;
+
+    double offset;
+
+    int rated;
+
+
+    static class VideoComparator implements Comparator<Video> {
+
+        @Override
+        public int compare(Video o1, Video o2) {
+            return o2.rated - o1.rated;
+        }
+    }
+
+
+    abstract void play(double offset);
+
+    abstract void stop();
+
+}
+
+
+class Inventory {
+
+
+    public static Inventory inventory = new Inventory();
+
+    private Inventory() {
+
+    }
+
+    public static Inventory getInstance() {
+        return inventory;
+    }
+
+    Map<Long, Video> map = new HashMap<>();
+
+
+}
+
+class User {
+
+    String email;
+
+    Map<Long, Double> watchlist = new HashMap<>();  // should be LRU
+
+    public void playVideo(Video video) {
+
+
+        if (watchlist.containsKey(video.id)) {
+
+            video.play(watchlist.get(video.id));
+        } else {
+
+            video.play(0);
+        }
+
+    }
+
+    public void stopPlay(Video video) {
+
+
+        video.stop();
+
+
+        long id = video.id;
+
+        watchlist.put(id, video.offset);
+
+    }
+}
+
