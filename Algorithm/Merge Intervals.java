1d7770c1d6ae056687334f9ac54f2af03b995446
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-06-29 08:29:43 -0400

    Update Merge Intervals.java

--- Algorithm/Merge Intervals.java
+++ Algorithm/Merge Intervals.java
@@ -1,1 +1,44 @@
 
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        
+        List<Interval> res = new ArrayList<>();
+        
+        if(intervals == null || intervals.size() == 0){
+            return res;
+        }
+        
+        
+        Collections.sort(intervals, (o1, o2) -> o1.start - o2.start);
+        
+        
+        Interval curr = intervals.get(0);
+        
+        for(int i = 1; i < intervals.size(); i++){
+            
+            Interval next = intervals.get(i);
+            
+            if(curr.end < next.start){
+                res.add(curr);
+                curr = next;
+            }else{
+                
+                curr.end = Math.max(curr.end, next.end);
+            }
+            
+        }
+        
+        res.add(curr);
+        return res;
+        
+    }
+}
