261f3516c295381224e24c730d5ea97a7b25c4a8
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-07-07 14:57:58 -0400

    Create Palindrome Number.java

---
+++ Algorithm/Palindrome Number.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x < 0 || (x != 0 && x % 10 == 0)){
+            return false;
+        }
+        
+        
+        int res = 0;
+        while(x > res){
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        
+        return res == x || res / 10 == x;
+        
+    }
+}
