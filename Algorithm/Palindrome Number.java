bbbee11d67210dd41a2a8264642fc8a2554e3958
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-07-07 15:20:06 -0400

    Update Palindrome Number.java

--- Algorithm/Palindrome Number.java
+++ Algorithm/Palindrome Number.java
@@ -16,3 +16,54 @@ public class Solution {
         
     }
 }
+
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Palindrome palindrome = new Palindrome();
+
+        System.out.println(palindrome.isPalindrome(9));
+    }
+
+
+    public boolean isPalindrome(int i) {
+
+
+        if (i < 0) {
+            return false;
+        }
+
+        String s = Integer.toBinaryString(i);
+
+        System.out.println(s);
+
+
+        return isPalindrome(s);
+
+
+    }
+
+
+    private boolean isPalindrome(String s) {
+
+
+        int st = 0;
+        int ed = s.length() - 1;
+
+        while (st <= ed) {
+
+            if (s.charAt(st) != s.charAt(ed)) {
+                return false;
+            }
+
+            st++;
+            ed--;
+        }
+
+        return true;
+    }
+
+
+}
