873e07391eecd9df4f7f0fe0d17c688261521ef0
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-07-25 20:08:59 -0400

    Create Wiggle Sort.java

---
+++ Algorithm/Wiggle Sort.java
@@ -0,0 +1,90 @@
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            // 需要交换的情况：奇数时nums[i] < nums[i - 1]或偶数时nums[i] > nums[i - 1]
+            if((i % 2 == 1 && nums[i] < nums[i-1]) || (i % 2 == 0 && nums[i] > nums[i-1])){
+                int tmp = nums[i-1];
+                nums[i-1] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+    }
+}
+
+
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        int[] tem = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            tem[i] = nums[i];
+        }
+        int mid = helper(tem, 0, nums.length-1, nums.length/2);
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = mid;
+        }
+        int l, r;
+        if (nums.length % 2 == 0) {
+            l = nums.length - 2;
+            r = 1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] < mid) {
+                    ans[l] = nums[i];
+                    l -= 2;
+                } else if (nums[i] > mid) {
+                    ans[r] = nums[i];
+                    r += 2;
+                }
+            }
+        } else {
+            l = 0;
+            r = nums.length - 2;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] < mid) {
+                    ans[l] = nums[i];
+                    l += 2;
+                } else if (nums[i] > mid) {
+                    ans[r] = nums[i];
+                    r -= 2;
+                }
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = ans[i];
+        }
+    }
+    public int helper(int[] nums, int l, int r, int k) {
+        if (l == r) {
+            return nums[l];
+        }
+        int position = partition(nums, l, r);
+        if (position == k) {
+            return nums[position];
+        } else if (position  < k) {
+            return helper(nums, position + 1, r, k);
+        }  else {
+            return helper(nums, l, position - 1, k);
+        }
+    }
+    public int partition(int[] nums, int l, int r) {
+        // 初始化左右指针和pivot
+        int left = l, right = r;
+        int pivot = nums[left];
+        
+        // 进行partition
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) {
+                right--;
+            }
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= pivot) {
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+        
+        // 返还pivot点到数组里面
+        nums[left] = pivot;
+        return left;         
+    }
+}
