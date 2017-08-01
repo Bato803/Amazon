c4830330ce4f4a1c363dfd98eb6d9f47d7739f91
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-08-01 11:54:09 -0400

    Update Number of Islands.java

--- Algorithm/Number of Islands.java
+++ Algorithm/Number of Islands.java
@@ -58,3 +58,83 @@ public class Solution {
 中心思想： 将CELL ID组合来表示一个岛(hash to string)，然后变形string, 最后往set里丢。
 
 
+
+public class IsLand2 {
+    public static void main(String[] args) {
+
+        IsLand2 island = new IsLand2();
+
+        int[][] pos = new int[][]{{0, 0}, {0, 1}, {1, 2}, {2, 1}};
+
+        System.out.println(island.isLand2(3, 3, pos));
+
+    }
+
+
+    public List<Integer> isLand2(int m, int n, int[][] pos) {
+
+        int[] rootArray = new int[m * n];
+
+
+        Arrays.fill(rootArray, -1);
+
+
+        int[] xs = new int[]{1, 0, -1, 0};
+
+        int[] ys = new int[]{0, 1, 0, -1};
+
+
+        List<Integer> res = new ArrayList<>();
+
+
+        int count = 0;
+
+        for (int i = 0; i < pos.length; i++) {
+
+
+            count++;
+
+
+            int index = pos[i][0] * n + pos[i][1];
+
+
+            rootArray[index] = index;
+
+            for (int j = 0; j < 4; j++) {
+
+                int x = xs[j] + pos[i][0];
+
+                int y = ys[j] + pos[i][1];
+
+
+                if (x >= 0 && x < m && y >= 0 && y < n && rootArray[x * n + y] != -1) {
+
+                    int root = getRoot(rootArray, x * n + y);
+
+                    if (root != index) {
+
+                        count--;
+
+                        rootArray[index] = root;
+                    }
+                }
+
+
+            }
+
+            res.add(count);
+        }
+
+        return res;
+
+    }
+
+    private int getRoot(int[] arr, int a) {
+
+        while (arr[a] != a) {
+            a = arr[a];
+        }
+
+        return a;
+    }
+}
