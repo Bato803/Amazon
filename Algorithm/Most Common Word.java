c071aab462b88b71e78e6b5bd8f066159a4d3b93
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2018-10-07 11:27:23 -0400

    Update Most Common Word.java

--- Algorithm/Most Common Word.java
+++ Algorithm/Most Common Word.java
@@ -37,3 +37,60 @@ class Solution {
         
     }
 }
+
+
+
+
+
+
+
+public class Solution {
+    /**
+     * @param s: a string
+     * @param excludewords: a dict
+     * @return: the most frequent word
+     */
+    public String frequentWord(String s, Set<String> excludewords) {
+        // Write your code here
+        if (s == null || s.isEmpty()) {
+            return null;
+        }
+        
+        Map<String, Integer> countMap = new HashMap<>();
+        int maxFreq = 0;
+        String result = null;
+        String[] words = s.split(" ");
+        for(String word : words) {
+            if (!Character.isLetter(word.charAt(word.length()-1))) {
+                word = word.substring(0, word.length()-1);
+            }
+            if (!excludewords.contains(word)){
+                countMap.put(word, countMap.getOrDefault(word, 0) + 1);
+                if (maxFreq < countMap.get(word)) {
+                    maxFreq = countMap.get(word);
+                    result = word;
+                } else if (maxFreq == countMap.get(word) && word.compareTo(result) < 0) {
+                    result = word;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
