f326e3f2ce9a5ac3a1cc43ea78d8d498367c622e
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2018-10-07 12:37:56 -0400

    Create Dot Product.java

---
+++ Algorithm/Dot Product.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     * @param A: an array
+     * @param B: an array
+     * @return: dot product of two array
+     */
+    public int dotProduct(int[] A, int[] B) {
+        // Write your code here
+        if (A == null || B == null || A.length == 0 || B.length == 0 || A.length != B.length) {
+            return -1;
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += (A[i] * B[i]);
+        }
+        return sum;
+    }
+}
