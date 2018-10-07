6854d9d0506524b5600cc9f22eca6098cbaa73b9
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2018-10-07 16:56:29 -0400

    Update LongestScreen.java

--- Algorithm/LongestScreen.java
+++ Algorithm/LongestScreen.java
@@ -1,3 +1,23 @@
+/*
+A string, each character representing a scene. Between two identical characters is considered to be a continuous scene. For example: abcda, you can think of these five characters as the same scene. Or acafghbeb can think of two aca and beb scenes. If there is a coincidence between the scenes, then the scenes are combined. For example, abcab, where abca and bcab are coincident, then the five characters are considered to be the same scene. Give a string to find the longest scene.
+
+1 <= |str| <=1e5
+str contains only lowercase letters
+Have you met this question in a real interview?  
+Example
+Given str = "abcda", return 5.
+
+Explanation:
+The longest scene is "abcda".
+Given str = "abcab", return 5.
+
+Explanation:
+The longest scene is "abcab".
+
+
+*/
+
+
 package am;
 
 import java.util.Arrays;
