ab2bf9cb2ab39e566a98b5d2f817aacf9195794f
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2018-10-07 11:43:07 -0400

    Update SameLengthNoRepeatSubString.java

--- Algorithm/SameLengthNoRepeatSubString.java
+++ Algorithm/SameLengthNoRepeatSubString.java
@@ -1,59 +1,19 @@
-package am;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public class SameLengthNoRepeatSubString {
-
-
-    public static void main(String[] args) {
-
-        String s = "abcdab";
-
-        System.out.println(new SameLengthNoRepeatSubString().getSubString(s, 4));
-    }
-
-
-    public List<String> getSubString(String s, int l) {
-
-
-        List<String> res = new ArrayList<>();
-
-
-        if (s == null || s.length() == 0) {
-
-            return res;
-        }
-
-
-        Set<Character> set = new HashSet<>();
-
-        int st = 0;
-        int ed = 0;
-
-        while (ed < s.length()) {
-
-            while (set.contains(s.charAt(ed))) {
-
-                set.remove(s.charAt(st));
-                st++;
+class Solution {
+    public int findSubstring(String s, int k) {
+            Set<String> stringSet = new HashSet<>();
+            int start = 0;
+            Set<Character> cSet = new HashSet<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                while (cSet.contains(c)) {
+                    cSet.remove(s.charAt(start++));
+                }
+                cSet.add(c);
+                if (cSet.size() == k) {
+                    stringSet.add(s.substring(start, i + 1));
+                    cSet.remove(s.charAt(start++));
+                }
             }
-
-            set.add(s.charAt(ed));
-            ed++;
-
-
-            if (ed - st == l) {
-
-                res.add(s.substring(st, ed));
-            }
-        }
-
-        return res;
-
-
-    }
-
+            return stringSet.size();
+    }       
 }
